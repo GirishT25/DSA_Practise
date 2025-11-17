a3f196769d15c5375405c64ad7c82d137b6b6f85
Girish <thoratgirish286@gmail.com>
2025-11-17 21:52:32 +0530

    Changes done with the Queue.cpp

--- STL/Queue.cpp
+++ STL/Queue.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 int main(){
 
-        
+    queue<int> q;
+    q.push(25);
+    q.push(35);
+    q.push(45);
+    q.push(55);
+    q.emplace(65);
+
+    cout<< q.front() << "\n";
+
+    q.pop();
+
+    cout<< q.front() << "\n";
+
+    cout<<q.back() << "\n";
 
 
     return 0;
